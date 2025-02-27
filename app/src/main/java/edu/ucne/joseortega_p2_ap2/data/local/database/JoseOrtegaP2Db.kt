a8e18220ab1a09a38889e82fad1cba124421bed0
Stiinger <ortegantony29@gmail.com>
2025-02-26 20:33:08 -0400

    Renombrando entidad

--- app/src/main/java/edu/ucne/joseortega_p2_ap2/data/local/database/JoseOrtegaP2Db.kt
+++ app/src/main/java/edu/ucne/joseortega_p2_ap2/data/local/database/JoseOrtegaP2Db.kt
@@ -3,13 +3,13 @@ package edu.ucne.joseortega_p2_ap2.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.joseortega_p2_ap2.data.local.dao.Dao
-import edu.ucne.joseortega_p2_ap2.data.local.entity.EntidadEntity
+import edu.ucne.joseortega_p2_ap2.data.local.entity.DepositoEntity
 
 @Database(
     entities = [
-        EntidadEntity::class,
+        DepositoEntity::class,
     ],
-    version = 1,
+    version = 5,
     exportSchema = false
 )
 abstract class JoseOrtegaP2Db : RoomDatabase() {
