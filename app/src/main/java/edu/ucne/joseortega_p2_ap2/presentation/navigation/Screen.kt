2ecb837ba9f9fe7cc2c7f10d1c06e854d345df68
Stiinger <ortegantony29@gmail.com>
2025-02-26 20:33:36 -0400

    Agregar Navegacion de Deposito

--- app/src/main/java/edu/ucne/joseortega_p2_ap2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/joseortega_p2_ap2/presentation/navigation/Screen.kt
@@ -3,7 +3,8 @@ package edu.ucne.joseortega_p2_ap2.presentation.navigation
 import kotlinx.serialization.Serializable
 
 sealed class Screen {
-    data object EntidadList : Screen()
     @Serializable
-    data class Entidad(val entidadId: Int) : Screen()
+    data object DepositoList : Screen()
+    @Serializable
+    data class Deposito(val depositoId: Int) : Screen()
 }
