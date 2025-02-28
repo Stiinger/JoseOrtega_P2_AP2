0e379135237f414f55f96dc1c516ddce39127b4d
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-02-28 05:57:23 -0400

    Eliminar método inutil GetDeposito

--- app/src/main/java/edu/ucne/joseortega_p2_ap2/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/joseortega_p2_ap2/data/remote/RemoteDataSource.kt
@@ -7,7 +7,6 @@ class RemoteDataSource @Inject constructor(
     private val ticketManagerApi: DepositoApi
 ) {
     suspend fun getDepositos() = ticketManagerApi.getDepositos()
-    suspend fun getDeposito(id: Int) = ticketManagerApi.getDeposito(id)
     suspend fun saveDeposito(depositoDto: DepositoDto) = ticketManagerApi.saveDeposito(depositoDto)
     suspend fun updateDeposito(id: Int, depositoDto: DepositoDto) = ticketManagerApi.updateDeposito(id, depositoDto)
     suspend fun deleteDeposito(id: Int) = ticketManagerApi.deleteDeposito(id)
